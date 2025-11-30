b5b14a25e3363abad654f5c47aee6bf1a2cb319c
Hoangnth <hoangnth@oceantech.com>
2025-11-30 19:31:00 +0700

    Fixbug

--- lab_management_app.py
+++ lab_management_app.py
@@ -1755,7 +1755,7 @@ def start_lab(lab_id):
         raise ValueError("No available port for lab!")
     print("===================== WEB TEST RUN IN PORT ", port)
     lab_session.success_start_lab_output = lab.output_result.replace("${webTestPort}", str(port))
-    lab_session.success_start_lab_output = lab.output_result.replace("${studentId}", user_id)
+    lab_session.success_start_lab_output = lab.output_result.replace("${studentId}", user_linux_name.replace("student_",""))
     print("===================== EXPECT OUTPUT RESULT ", lab_session.success_start_lab_output)
     try:
         db.session.commit()
