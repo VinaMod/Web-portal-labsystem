a07685b48e4b60f17292005dcb749b643bd0df5c
Hoangnth <hoangnth@oceantech.com>
2025-12-27 13:27:24 +0700

    Add event emmitter

--- lab_management_app.py
+++ lab_management_app.py
@@ -1862,7 +1862,7 @@ def run_lab_commands(lab_session_id):
                 print(f"Executing run command: {replaced_command}")
                 execute_run_command(user_linux_name, replaced_command, lab_session.student_folder)
 
-        socketio.emit('terminal_output', { lab_session_id: lab_session.id })
+        socketio.emit('start_terminal', { lab_session_id: lab_session.id })
         return jsonify({'message': 'Lab commands executed successfully'})
     except Exception as e:
         print(f"Error running lab commands: {e}")
