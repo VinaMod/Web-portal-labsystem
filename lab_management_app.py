35ba1882a3477b0b9abda196f25fe0ecfaa357e6
Hoangnth <hoangnth@oceantech.com>
2025-12-27 13:14:17 +0700

    Add log

--- lab_management_app.py
+++ lab_management_app.py
@@ -2692,6 +2692,7 @@ def handle_start_terminal(data):
     linux_username = get_student_username(user.email)
     working_dir = f'/home/{linux_username}' or '/tmp'
     values =start_command_param if json.loads(start_command_param.parameter_values) else None
+    print("================= PARAMETERS: ", values)
     start_command = values if values[0] else None
     if not start_command:
         start_command = f'cd {working_dir} && newgrp {linux_username}'
